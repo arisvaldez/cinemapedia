04bae3925de645fed5be78180a269bd3563b4ceb
Aris Valdez <samus45@hotmail.es>
2023-08-20 00:00:13 -0400

    remove dotenv import from home_screen

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = "home-screen";
